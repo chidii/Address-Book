d238efecf691c43dbdc9ecefbb4809f82ead590b
Olufemi T Sam <olufemititilayosamuel@gmail.com>
2018-04-18 17:17:16 -0700

    Address Book Project

---
+++ Address Book/main.js
@@ -0,0 +1,9 @@
+(function(global) {
+
+    var store = global.store;
+    store.addContact(store.ContactInfo('onyenwe', '080000000000', 'onyenwejennifer04@gmail.com'));
+    store.addContact(store.ContactInfo('chidinma', '02-9331232', 'james@yahoo.com'));
+    store.addContact(store.ContactInfo('andela', '03-9331313', 'judy@hotmail.com'));
+
+}(this));
+
